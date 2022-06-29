505f39e286d0a8f0b36ab967e49c7b55f8ea43da
Jarmarr Washington <jarmarrwashington@Jarmarrs-iMac.local>
2022-06-29 19:00:48 -0400

    try to fix code

--- frontend/src/contexts/CurrentUser.js
+++ frontend/src/contexts/CurrentUser.js
@@ -3,14 +3,14 @@ import { createContext, useState, useEffect } from "react";
 
 export const CurrentUser = createContext()
 
-
 function CurrentUserProvider({ children }) {
 
     const [currentUser, setCurrentUser] = useState(null)
+
     useEffect(() => {
 
         const getLoggedInUser = async () => {
-            let response = await fetch('http://localhost:5000/authentication/profile',{
+            let response = await fetch('http://localhost:5000/authentication/profile', {
                 credentials: 'include'
             })
             let user = await response.json()
@@ -18,10 +18,7 @@ function CurrentUserProvider({ children }) {
         }
         getLoggedInUser()
     }, [])
-  
 
-    
-    
     return (
         <CurrentUser.Provider value={{ currentUser, setCurrentUser }}>
             {children}
